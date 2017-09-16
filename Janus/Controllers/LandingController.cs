3ccf9a09d41a69a31079a4ff5e49f59c782f9fbb
Matt Martin <mmartin7064@gmail.com>
2017-09-16 19:12:10 -0400

    Created Database and Started Landing Page views

---
+++ Janus/Controllers/LandingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Janus.Controllers
+{
+    public class LandingController : Controller
+    {
+        // GET: CreateAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
