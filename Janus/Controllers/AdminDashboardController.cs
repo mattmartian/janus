3b856e2f54d9b086a707248aca26318e8bc98043
Matthew Martin <mmartin7064@gmail.com>
2017-11-20 02:03:04 -0500

    Employee Management Page in Admin Panel Finished

--- Janus/Controllers/AdminDashboardController.cs
+++ Janus/Controllers/AdminDashboardController.cs
@@ -9,7 +9,6 @@ namespace Janus.Controllers
 {
     public class AdminDashboardController : Controller
     {
-        private Janus.Models.UserDB userDB = new Janus.Models.UserDB();
         private readonly JanusEntities _context;
         // GET: AdminDashboard
 
@@ -20,12 +19,93 @@ namespace Janus.Controllers
 
         public ActionResult AdminDashboard()
         {
-            return View();
+            return RedirectToAction("ManageEmployees", "AdminDashboard");
         }
 
         public ActionResult ManageEmployees()
         {
+            var employeeData = (from a in _context.Users select new Janus.Models.EmployeeDetailViewModel { firstName = a.firstName, lastName = a.lastName, role = a.role, departmentName = a.departmentName, userID = a.userID });
+            ViewBag.data = employeeData;
+
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            var v = _context.Users.Where(a => a.userID == id).FirstOrDefault();
+            return View(v);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Users emp)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (_context)
+                {
+                    if (emp.userID > 0)
+                    {
+                        //Edit
+                        var v = _context.Users.Where(a => a.userID == emp.userID).FirstOrDefault();
+                        if (v != null)
+                        {
+                            v.firstName = emp.firstName;
+                            v.lastName = emp.lastName;
+                            v.role = emp.role;
+                            v.departmentName = emp.departmentName;
+                        }
+                    }
+                    else
+                    {
+                        //Save
+                        _context.Users.Add(emp);
+                    }
+                    _context.SaveChanges();
+                    status = true;
+                }
+            }
+            return RedirectToAction("ManageEmployees", "AdminDashboard");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (_context)
+            {
+                var v = _context.Users.Where(a => a.userID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            bool status = false;
+            using (_context)
+            {
+                var v = _context.Users.Where(a => a.userID == id).FirstOrDefault();
+                var c = _context.Availibility.Where(a => a.userID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    _context.Availibility.Remove(c);
+                    _context.SaveChanges();
+                    _context.Users.Remove(v);
+                    _context.SaveChanges();
+
+                    status = true;
+                }
+            }
+            return RedirectToAction("ManageEmployees", "AdminDashboard");
         }
 
         public ActionResult ManageRequests()
---
+++ Janus/Models/EmployeeDetailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Janus.Models
+{
+    public class EmployeeDetailViewModel
+    {
+        public int userID { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string role { get; set; }
+
+        public string departmentName { get; set; }
+    }
+}
